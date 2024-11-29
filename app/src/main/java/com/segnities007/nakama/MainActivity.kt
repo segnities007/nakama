026a47b7b982c5a10ddbf215e782bdfeb57667d2
seg <segnities007@gmail.com>
2024-11-30 02:33:10 +0900

    more

--- app/src/main/java/com/segnities007/nakama/MainActivity.kt
+++ app/src/main/java/com/segnities007/nakama/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.segnities007.nakama.ui.theme.NakamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,17 +35,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+private fun Router(){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "/",
+
+    ){
+        composable("/"){
+            Splash()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NakamaTheme {
-        Greeting("Android")
-    }
 }
