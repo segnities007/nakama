bd3796efb2ba773f07800489998842db760f5e69
seg <segnities007@gmail.com>
2024-12-16 00:39:11 +0900

    create logic ui

--- app/src/main/java/com/segnities007/nakama/data/model/headers/HeaderLogin.kt
+++ app/src/main/java/com/segnities007/nakama/data/model/headers/HeaderLogin.kt
@@ -3,7 +3,7 @@ package com.segnities007.nakama.data.model.headers
 import com.segnities007.nakama.R
 
 data class HeaderLogin(
-    override val navigationIcon: Int = R.drawable.baseline_menu_24,
+    override val navigationIcon: Int = 0,
     override val actionIcon: Int = 0,
     override val title: String = "Nakama",
 ): Header
--- app/src/main/java/com/segnities007/nakama/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/segnities007/nakama/ui/screens/login/LoginViewModel.kt
@@ -1,6 +1,7 @@
 package com.segnities007.nakama.ui.screens.login
 
 import androidx.compose.material3.DrawerValue
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import com.segnities007.nakama.data.model.drawer.DrawerViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,11 +13,24 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
 
 ): ViewModel(), DrawerViewModel {
+
     private val _index = MutableStateFlow(0)
     override val index: StateFlow<Int> = _index
-
     private var _drawerState = MutableStateFlow<DrawerValue>(DrawerValue.Closed)
     override val drawerState: StateFlow<DrawerValue> = _drawerState
+
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password
+
+    fun updateEmail(value: String){
+        _email.value = value
+    }
+
+    fun updatePassword(value: String){
+        _password.value = value
+    }
 
     override fun openDrawer(){
         _drawerState.value = DrawerValue.Open
